067635538b842d1543c411bc67837d9594e1c9ab
Kunlexi <adesinataofik5@gmail.com>
2025-04-13 12:46:31 +0100

    view balance

--- Models/Generators.cs
+++ Models/Generators.cs
@@ -49,4 +49,14 @@ public class Generators
 
         return number;
     }
+
+
+    /* This method find the account with the given account number, and
+     * Returns the balance of the found account.
+     */
+    pubblic static double ViewAccountBalance(List<Account> accounts, string accountNumber)
+    {
+        var account = accounts.SingleOrDefault(a => a.AccountNumber == accountNumber);
+        return account.Balance;
+    }
 }
