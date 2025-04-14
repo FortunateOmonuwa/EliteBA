e0cd129246ec1a0090c55da963b61f645dc19df6
Kunlexi <adesinataofik5@gmail.com>
2025-04-14 04:03:36 +0100

    view balance update

---
+++ Funtionalities/BankOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using system.Linq;
+using EliteBA.Models;
+
+namespace EliteBA.Functionalities
+{
+	public static class BankOperations
+	{
+        /* This method find the account with the given account number, and
+         * Returns the balance of the found account.
+         */
+        public static double ViewAccountBalance(List<Account> accounts, string accountNumber)
+        {
+            var account = accounts.SingleOrDefault(a => a.AccountNumber == accountNumber);
+           
+            return account.Balance;
+        }
+    }
+}
+
--- Models/Generators.cs
+++ Models/Generators.cs
@@ -51,12 +51,4 @@ public class Generators
     }
 
 
-    /* This method find the account with the given account number, and
-     * Returns the balance of the found account.
-     */
-    pubblic static double ViewAccountBalance(List<Account> accounts, string accountNumber)
-    {
-        var account = accounts.SingleOrDefault(a => a.AccountNumber == accountNumber);
-        return account.Balance;
-    }
 }
