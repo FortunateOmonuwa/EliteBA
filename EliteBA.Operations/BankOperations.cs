854578548944852b69428b904dc123e8fda00fe9
Kunlexi <adesinataofik5@gmail.com>
2025-04-29 09:28:11 +0100

    view balance

--- EliteBA.Operations/BankOperations.cs
+++ EliteBA.Operations/BankOperations.cs
@@ -4,5 +4,23 @@ namespace EliteBA.Operations;
 
 public class BankOperations
 {
-    
+    /**
+     * This method finds the account with the given account number, and
+     * Returns the balance of the found account.
+     * If the account is not found, it returns -1. (Or you might throw an exception)
+     */
+    public static double ViewAccountBalance(List<Account> accounts, string accountNumber)
+    {
+        var account = accounts.SingleOrDefault(a => a.AccountNumber == accountNumber);
+
+        if (account != null)
+        {
+            return account.Balance;
+        }
+        else
+        {
+            Console.WriteLine($"Account with number {accountNumber} not found.");
+            return 0; // Or throw an exception
+        }
+    }
 }
