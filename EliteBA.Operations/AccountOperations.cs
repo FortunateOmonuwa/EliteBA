8f70ee35f7a57870edebecd0e090109a1d14f00c
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-04-29 12:54:36 +0100

    added transfer logic

--- EliteBA.Operations/AccountOperations.cs
+++ EliteBA.Operations/AccountOperations.cs
@@ -1,10 +1,44 @@
 ﻿using EliteBA.DB;
 using EliteBA.Models;
+using EliteBA.Utilities;
 
 namespace EliteBA.Operations;
 
 public class AccountOperations
 {
+    public record TransferDTO(string senderAcc, string receiver, double amount);
+    public string Transfer(TransferDTO transferDetails)
+    {
+        var senderAcc = Tables.accounts.SingleOrDefault(x => x.AccountNumber == transferDetails.senderAcc);
+        var receiverAcc = Tables.accounts.SingleOrDefault(x => x.AccountNumber == transferDetails.receiver);
+
+        if(senderAcc == null)
+        {
+            return $"{transferDetails.senderAcc} does not exist";
+        }
+        if (receiverAcc == null)
+        {
+            return $"{transferDetails.receiver} does not exist";
+        }
+
+        senderAcc.Balance -= transferDetails.amount;
+        receiverAcc.Balance += transferDetails.amount;
+
+        var transcation = new Transaction
+        {
+            AccountId = senderAcc.AccountId,
+            Amount = transferDetails.amount,
+            RecipientAccountId = receiverAcc.AccountId,
+            DateCreated = DateTime.Now,
+            Narration = $"Transfer from {senderAcc.AccountName} to {receiverAcc.AccountName}",
+            TransactionId = Generators.GenerateTransactionId(),
+            IsTransfer = true,
+            TransactionType = TransactionType.Transfer,
+        };
+        Tables.transactions.Add(transcation );
+
+        return "Transaction successful";  
+    }
     /**
    * This method handles the generation of account numbers.
    * The method generates a random 10 digits string and compares with existing accounts list to ensure it is unique.
