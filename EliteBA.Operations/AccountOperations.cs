cb75efe0fcc2d20fc1e2e22b5f9bce5bc538cf75
sekinah <sekinahdeleoladepo@gmail.com>
2025-05-05 15:45:44 +0100

    TransferUpdate

--- EliteBA.Operations/AccountOperations.cs
+++ EliteBA.Operations/AccountOperations.cs
@@ -21,32 +21,10 @@ public class AccountOperations
         {
             for (int i = 0; i < 10; i++) accountNumber += random.Next(0, 9);
         } while (Tables.accounts.Any(account => account.AccountNumber == accountNumber));
-        
-        return accountNumber;
-    }
-    
-    
-    /// <summary>
-    /// This Method creates a new bank account using the details provided in the CreateAccountDto.
-    /// 
-    /// </summary>
-    /// <param name="dto"></param>
-    /// <returns></returns>
-    internal static Account CreateAccount(CreateAccountDto dto)
-    {
-        var accountNumber = GenerateAccountNumber();
 
-        //This converts the accountType string to enum
-        AccountType parsedAccountType = (AccountType)Enum.Parse(typeof(AccountType), dto.accountType, true);
-        var account = new Account
-        {
-            AccountName = $"{dto.lastname} {dto.firstname}",
-            AccountType = parsedAccountType,
-            AccountNumber = accountNumber
-        };
-        //Now we add our object to the account List
-        Tables.accounts.Add(account);
-        return account;
+        return accountNumber;
     }
 
 }
+        
+    
