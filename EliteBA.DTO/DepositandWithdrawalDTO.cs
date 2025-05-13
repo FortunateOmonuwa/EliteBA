b3658026c4f2091ae18bd8ae7236ad75fe080332
ogbu Wisdom <Wisdomsure5@gmail.com>
2025-05-13 11:53:04 +0100

    add implementation to withdraw and deposit

---
+++ EliteBA.DTO/DepositandWithdrawalDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliteBA.DTO
+{
+    public class DepositandWithdrawalDTO
+    {
+        public string receiversAccId { get; set; }
+        public double Amount { get; set; }
+    }
+    
+}
